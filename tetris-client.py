dfa17bb3f42168550d2ba4e1f9c6421cbc2839ad
heeeeeng <boh_zhu@163.com>
2018-07-20 19:13:04 +0800

    some update

--- tetris-client.py
+++ tetris-client.py
@@ -1,6 +1,9 @@
 
 import json
 import requests
+import time
+import queue
+import threading
 from pynput import keyboard
 from pynput.keyboard import Key
 
@@ -10,9 +13,94 @@ SERVER_PORT     = "10000"
 class Client():
 
     def __init__(self):
-        self.init_keyboard()
         self.init_network()
         self.init_game_info()
+
+        self.tasks = []
+
+    def start(self):
+        q = queue.Queue()
+
+        gui = ClientGUI(q)
+        t_gui = threading.Thread(target=gui.start)
+        t_gui.daemon = True
+        t_gui.start()
+
+        controller = ClientController(q, self.game_id, self.user_id)
+        t_ctrl = threading.Thread(target=controller.start)
+        t_ctrl.daemon = True
+        t_ctrl.start()
+
+        self.tasks.append(gui)
+        self.tasks.append(controller)
+
+        q.get()
+        self.stop()
+            
+
+    def stop(self):
+        for t in self.tasks:
+            t.stop()
+
+    def init_network(self):
+        pass
+
+    def init_game_info(self):
+        self.game_id = "gameid"
+        self.user_id = "userid"
+
+        self.tetris_metrix = []
+
+    """
+    communication protocal:
+    {
+        "gameID": gameid,
+        "userID": userid,
+        "action": action
+    }
+    **action** :
+    space   ->  0
+    up      ->  1
+    down    ->  2
+    left    ->  3
+    right   ->  4
+    """
+
+
+class ClientGUI():
+
+    def __init__(self, q):
+        self.shoud_stop_q = q
+
+        self.fps = 30
+        self._is_running = True
+
+    def start(self):
+        
+        while self._is_running:
+            time.sleep(1 / self.fps)
+            self.refresh()
+            self.draw()
+
+    def stop(self):
+        self._is_running = False
+
+    def refresh(self):
+        pass
+
+    def draw(self):
+        pass
+
+
+class ClientController():
+
+    def __init__(self, q, gameid, userid):
+        self.shoud_stop_q = q
+
+        self.game_id = gameid
+        self.user_id = userid
+
+        self.init_keyboard()
 
     def start(self):
         with keyboard.Listener(
@@ -23,24 +111,10 @@ class Client():
     def stop(self):
         keyboard.Listener.stop(None)
 
-    """
-    communication protocal:
-    {
-        "gameID": gameid,
-        "userID": userid,
-        "action": action,
-        "gameMetrix": gameMetrix
-    }
-    **action** :
-    space   ->  0
-    up      ->  1
-    down    ->  2
-    left    ->  3
-    right   ->  4
-    """
     def controller(self, key):
         if key == Key.esc:
-            self.stop()
+            self.shoud_stop_q.put(True)
+            return
 
         action = self.key_map.get(key, -1)
         if action >= 0:
@@ -50,23 +124,14 @@ class Client():
         req_data = {
             "gameID": self.game_id,
             "userID": self.user_id,
-            "action": action,
-            "gameMetrix": self.tetris_metrix
+            "action": action
         }
-        URL = "http://" + SERVER_URL + ":" + SERVER_PORT
-        r = requests.post(URL, data=json.dumps(req_data))
+        # URL = "http://" + SERVER_URL + ":" + SERVER_PORT
+        # r = requests.post(URL, data=json.dumps(req_data))
 
         # TODO
-        print(r.text)
-
-    def init_network(self):
-        pass
-
-    def init_game_info(self):
-        self.game_id = "gameid"
-        self.user_id = "userid"
-
-        self.tetris_metrix = []
+        # print(r.text)
+        print(req_data)
 
 
     def init_keyboard(self):
